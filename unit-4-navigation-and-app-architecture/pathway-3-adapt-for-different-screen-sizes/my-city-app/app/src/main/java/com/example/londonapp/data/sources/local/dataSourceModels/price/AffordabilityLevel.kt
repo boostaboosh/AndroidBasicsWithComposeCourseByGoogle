a0658662860bb62724dd55802d15d99b0844e4cf
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2025-03-04 07:51:38 +0000

    Add AffordabilityLevel toCapitalisedString

--- unit-4-navigation-and-app-architecture/pathway-3-adapt-for-different-screen-sizes/my-city-app/app/src/main/java/com/example/londonapp/data/sources/local/dataSourceModels/price/AffordabilityLevel.kt
+++ unit-4-navigation-and-app-architecture/pathway-3-adapt-for-different-screen-sizes/my-city-app/app/src/main/java/com/example/londonapp/data/sources/local/dataSourceModels/price/AffordabilityLevel.kt
@@ -1,8 +1,16 @@
 package com.example.londonapp.data.sources.local.dataSourceModels.price
 
+import java.util.Locale
+
 enum class AffordabilityLevel {
     EXPENSIVE,
     NORMAL,
     CHEAP,
     FREE
+}
+
+fun AffordabilityLevel.toCapitalisedString() : String {
+    return this.name
+        .lowercase(Locale.ROOT)
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
 }

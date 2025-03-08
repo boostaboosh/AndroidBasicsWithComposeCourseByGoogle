9f0ed96a35db9405cc450946cfe3bdca280c77b4
boostaboosh <81049475+boostaboosh@users.noreply.github.com>
2025-03-08 18:27:02 +0000

    Add border and elevation to menu card

--- unit-4-navigation-and-app-architecture/pathway-3-adapt-for-different-screen-sizes/my-city-app/app/src/main/java/com/example/londonapp/ui/stateConsumers/screens/MenuScreen.kt
+++ unit-4-navigation-and-app-architecture/pathway-3-adapt-for-different-screen-sizes/my-city-app/app/src/main/java/com/example/londonapp/ui/stateConsumers/screens/MenuScreen.kt
@@ -1,15 +1,19 @@
 package com.example.londonapp.ui.stateConsumers.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -72,8 +76,15 @@ fun PlaceCategoryCard(
 ) {
     Card(
         onClick = onClick,
-        modifier = modifier
-            .fillMaxWidth(),
+        border = BorderStroke(
+            width = 1.dp,
+            Color(red = 0x00, green = 0x00, blue = 0x00, alpha = 0xFF)
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp,
+            pressedElevation = 8.dp,
+        ),
+        modifier = modifier.fillMaxWidth(),
     ) {
         Text(
             text = text,
